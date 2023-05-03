7497bde9bbc55e773c48436602f97a510e16e308
CllaudiaB <buraanna27@gmail.com>
2023-05-03 07:19:41 +0000

    update file

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,7 @@
 docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04
 apt-get update && apt-get install -y hugo make
-apt-get install wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
+apt-get install wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
 tar -xvzf hugo_0.84.0_Linux-64bit.tar.gz
 sudo mv hugo /usr/local/bin/
 make build
